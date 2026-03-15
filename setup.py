4e391c21179c2575e5189b2ac42cf722d498f08b
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-03-15 17:31:41 +0800

    alvistack/6.2.2
        git clean -xdf
        tar zcvf ../python-croniter_6.2.2.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-croniter.spec ../python-croniter_6.2.2-1.spec
        cp ../python*-croniter*6.2.2*.{gz,xz,spec,dsc} /osc/home\:alvistack/pallets-eco-croniter-6.2.2/
        rm -rf ../python*-croniter*6.2.2*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='croniter',
+    version='6.2.2',
+    description='croniter provides iteration for datetime object with cron like format',
+    author_email='Matsumoto Taichi <taichino@gmail.com>, kiorky <kiorky@cryptelium.net>, Ash Berlin-Taylor <ash@apache.org>, Jarek Potiuk <jarek@potiuk.com>',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
+    install_requires=[
+        'python-dateutil',
+    ],
+    packages=[
+        'croniter',
+        'croniter.tests',
+    ],
+    package_dir={'': 'src'},
+)
